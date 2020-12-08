6189324d098eb1147257d3255a876d9f1d24aadf
Josh Cheung <JTCheung3@gmail.com>
2020-12-07 20:36:59 -0800

    Updates

---
+++ precache-manifest.c5a610cfdd4c9ce14702078f2f53a2c0.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cec7a857c775aac3dc344c84a9a0058",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d6a943d0dcbffd9c681",
+    "url": "/static/css/main.0dec6cf5.chunk.css"
+  },
+  {
+    "revision": "b129aba346a6eef15208",
+    "url": "/static/js/2.c329f6f0.chunk.js"
+  },
+  {
+    "revision": "c83be2e7cfaa63cef32980cc4ea7c407",
+    "url": "/static/js/2.c329f6f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d6a943d0dcbffd9c681",
+    "url": "/static/js/main.502839a7.chunk.js"
+  },
+  {
+    "revision": "42fccb107ddbcc362233",
+    "url": "/static/js/runtime-main.dbf410c9.js"
+  },
+  {
+    "revision": "4a9f98f9936280c0e36560a3381b3c13",
+    "url": "/static/media/AboutMe.4a9f98f9.png"
+  },
+  {
+    "revision": "c7bd33988719cd52a5c1125856ed0f98",
+    "url": "/static/media/Anita-Kuo-2020-Resume.c7bd3398.pdf"
+  },
+  {
+    "revision": "85ed2986f7c88b9961cf6d8cb31bda16",
+    "url": "/static/media/AnitaKuo.85ed2986.png"
+  },
+  {
+    "revision": "8db1584d60f7c9ce07381723849864c0",
+    "url": "/static/media/Contacts.8db1584d.png"
+  },
+  {
+    "revision": "2db0bed830f3bd2cf3204a0678626ceb",
+    "url": "/static/media/Hobbies2.2db0bed8.png"
+  },
+  {
+    "revision": "5197a761b579af6a807ba1dcd49223bb",
+    "url": "/static/media/Recordings.5197a761.png"
+  },
+  {
+    "revision": "edd566dc18fbdf674138498ab4234215",
+    "url": "/static/media/Repertoire.edd566dc.png"
+  },
+  {
+    "revision": "0161191f7974ac7a76494ec744d7aaa4",
+    "url": "/static/media/beef-dish.0161191f.jpg"
+  },
+  {
+    "revision": "3e8ce990a3c0773843468b86d35165fb",
+    "url": "/static/media/black-sesame-loaf.3e8ce990.jpg"
+  },
+  {
+    "revision": "67eb73746b24ff89f54ae610264bfbae",
+    "url": "/static/media/cheesecake.67eb7374.jpg"
+  },
+  {
+    "revision": "616a61351b94b25df0447ac20ae35124",
+    "url": "/static/media/contacts-logo.616a6135.png"
+  },
+  {
+    "revision": "48b9e83c8b07548aedff30f344978c18",
+    "url": "/static/media/creampuffs-cross-section.48b9e83c.jpg"
+  },
+  {
+    "revision": "91df9698df227ecf100a9b2f2199829f",
+    "url": "/static/media/creampuffs.91df9698.jpg"
+  },
+  {
+    "revision": "36a04514d8c801550a0e0c4ece7b612e",
+    "url": "/static/media/egg-custards.36a04514.jpg"
+  },
+  {
+    "revision": "98acc249fd7008e7b83f3f3353c485f3",
+    "url": "/static/media/fresh-pasta.98acc249.jpg"
+  },
+  {
+    "revision": "4ab469e35bb00335b7ef273d4b92f261",
+    "url": "/static/media/hobbies-logo2.4ab469e3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9cb3fb25a83a8e95108f746411dfd0cf",
+    "url": "/static/media/logo.9cb3fb25.png"
+  },
+  {
+    "revision": "ce429575ae010dd963a73a70d5c70edf",
+    "url": "/static/media/mantou.ce429575.jpg"
+  },
+  {
+    "revision": "7703575b139a2333acccd107bee1a2a1",
+    "url": "/static/media/performances-logo.7703575b.png"
+  },
+  {
+    "revision": "b51c6936aae0eab85609cf5f5ecc078c",
+    "url": "/static/media/profile-pic.b51c6936.jpg"
+  },
+  {
+    "revision": "23667a3cc64858ed8556808e06c085ac",
+    "url": "/static/media/raspberry-pie.23667a3c.jpg"
+  },
+  {
+    "revision": "bf5b616ad6c3649aafa109c15d68d98f",
+    "url": "/static/media/repertoire-logo.bf5b616a.png"
+  },
+  {
+    "revision": "e702659a59cf853f8e024785c22fc7c5",
+    "url": "/static/media/sandwich.e702659a.jpg"
+  },
+  {
+    "revision": "a622d6eb5fce2ffc18104bd7739a7e9e",
+    "url": "/static/media/secondary-picture.a622d6eb.jpg"
+  },
+  {
+    "revision": "879ebe01bac4b7caae038be2f1d72fbb",
+    "url": "/static/media/sourdough.879ebe01.jpg"
+  },
+  {
+    "revision": "7fda006315c1de0b024d10760c854971",
+    "url": "/static/media/temp-profile.7fda0063.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AnitaKuo/precache-manifest.1ccd205ec7cf4146c63c5b001b41d31e.js"
+  "/precache-manifest.c5a610cfdd4c9ce14702078f2f53a2c0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AnitaKuo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
